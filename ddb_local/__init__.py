55282d682abaeaff57871135dabc3ea0d841e0c0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-20 22:50:28 +0000

    Adding tarfile member sanitization to extractall()

--- ddb_local/__init__.py
+++ ddb_local/__init__.py
@@ -84,7 +84,26 @@ class LocalDynamoDB(object):
             with requests.get(self.source_url, stream=True) as req:
                 req.raise_for_status()
                 with tarfile.open(fileobj=req.raw, mode="r:gz") as tf:
-                    tf.extractall(path=self.unpack_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tf, path=self.unpack_dir)
 
     def _ensure_port_free(self):
         try:
